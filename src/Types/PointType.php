5356c2dc41dce48160daf0ee42a21f6f26b846bf
dreamsbond <info@dreamsbond.com>
2020-04-29 15:50:22 +0800

    update.

--- src/Types/PointType.php
+++ src/Types/PointType.php
@@ -2,6 +2,9 @@
 
 namespace Orzford\Limoncello\Spatial\Types;
 
+use Brick\Geo\Proxy\PointProxy;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
 /**
  * @package App
  */
@@ -11,4 +14,17 @@ class PointType extends \Brick\Geo\Doctrine\Types\PointType
      * Type name
      */
     const NAME = 'limoncelloPoint';
+
+    /**
+     * @inheritDoc
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        /** @var PointProxy $phpValue */
+        $phpValue = parent::convertToPHPValue($value, $platform);
+
+        return $phpValue->asText();
+    }
+
+
 }
